ce9d732b48ccc6c9b2c52405c8dc8c54b5b8ea2f
anujkumar98 <anujcs2020@gmail.com>
2019-06-09 20:42:31 +0530

    Firebase working

---
+++ code/js/login.js
@@ -0,0 +1,28 @@
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        var emailVerified=user.emailVerified;
+        if(emailVerified){
+            window.open("map.html","_self");
+        }
+    } else {
+
+    }
+});
+function login() {
+    var email = document.getElementById('emailLogin').value;
+    var password = document.getElementById('passwordLogin').value;
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorCode + " " + errorMessage);
+    });
+    // var user = firebase.auth().currentUser;
+    // var emailVerified=user.emailVerified;
+    //     if(emailVerified){
+    //         window.open("map.html","_self");
+    //     }
+    //     else{
+    //         alert("Verify your email.");
+    //     }
+    window.open("map.html","_self");
+}
